3ca8a4f7d9d4b6b5056efa8b400e1d96f037e18d
Gonzalez Noe <Noe.Gonzalez@hlramerica.com>
2022-09-12 19:49:42 -0400

    Components and service created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipeDetailComponent } from './components/recipe-detail/recipe-detail.component';
+import { FilterRecipeComponent } from './components/filter-recipe/filter-recipe.component';
+import { ListRecipeComponent } from './components/list-recipe/list-recipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeDetailComponent,
+    FilterRecipeComponent,
+    ListRecipeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/filter-recipe/filter-recipe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-recipe',
+  templateUrl: './filter-recipe.component.html',
+  styleUrls: ['./filter-recipe.component.css']
+})
+export class FilterRecipeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
