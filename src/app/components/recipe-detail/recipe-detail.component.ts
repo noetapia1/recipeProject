e0b8f34b4a2b3f22a64ab1b56f137d51ee9251ed
Gonzalez Noe <Noe.Gonzalez@hlramerica.com>
2022-09-15 17:56:25 -0400

    Adding recipe detail

--- src/app/components/recipe-detail/recipe-detail.component.ts
+++ src/app/components/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Recipe } from 'src/app/interfaces/recipe';
+import { RecipeService } from 'src/app/service/recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeDetailComponent implements OnInit {
 
-  constructor() { }
+  recipe:Recipe | undefined;
+
+  constructor(private service:RecipeService, private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(
+      (params) =>{
+        this.getRecipeDetail(String(params.get("id")));
+      }
+    );
+  }
+
+  getRecipeDetail(id:string){
+    this.service.getRecipeDetail(id).subscribe(
+      (data)=>this.recipe = data
+    );
+  }
+
+  getIngredientImageUrl(url:string){
+    return "https://spoonacular.com/cdn/ingredients_100x100/" + url;
   }
 
 }
